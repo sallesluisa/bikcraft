69115b3e0588f0e67e412d40327ab435688387c1
Luísa Salles <luisacarvalhosalles@gmail.com>
2023-06-23 17:09:31 -0300

    add animação

---
+++ js/script.js
@@ -0,0 +1,70 @@
+//ATIVAR LINKS DO MENU
+
+const links = document.querySelectorAll(".header__menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo")
+  }
+}
+
+links.forEach(ativarLink);
+
+// ATIVAR LINKS DO ORÇAMENTO
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+//PERGUNTAS FREQUENTES
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativerPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativerPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// GALERIA DE BICICLETAS
+const galeria = document.querySelectorAll('.bicicletas__imgs img');
+const galeriaContainer = document.querySelector('.bicicletas__imgs');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media){
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//ANIMAÇÃO
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
